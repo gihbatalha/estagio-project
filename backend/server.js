1dfbfcc8a0b6be3a37833920e79d5e562c049077
Giovanna <giovanna.batalha03@gmail.com>
2019-05-09 15:11:54 -0300

    :tada: Initial Commit - Backend

---
+++ backend/server.js
@@ -0,0 +1,69 @@
+const express = require("express");
+const app = express();
+
+const bodyParser = require("body-parser");
+const MongoClient = require("mongodb").MongoClient;
+const ObjectId = require("mongodb").ObjectId;
+const cors = require('cors')
+
+app.use(cors())
+app.use(bodyParser.urlencoded({extended:true}))
+app.use(bodyParser.json());
+
+const uri = "mongodb+srv://estagioYuri2019:estagio2019@cluster0-opccq.mongodb.net/test?retryWrites=true";
+
+MongoClient.connect(uri, (err, client) => {
+    if (err) return console.log(err)
+    category = client.db("estagioYuri2019").collection("category");
+
+    app.listen(3000, function(){
+        console.log("Servidor inicializado na porta 3000");
+    });
+});
+
+app.post('/category', (req,res) => {
+    console.log("Salvando nova categoria: ", req.body);
+    category.insertOne(req.body)
+        .then(response => {
+            res.status(200).send(response);
+        }).catch(error => {
+            res.status(500).send("Error: "+ error);
+        });
+});
+
+app.get('/category', (req,res) => {
+    console.log("Recuperando todas as categorias");
+
+    category.find()
+        .toArray()
+        .then(results => {
+            res.status(200).send(results);
+        }).catch(error => {
+            res.status(500).send("Error: "+ error);
+        });
+});
+
+app.get('/category/:id', (req,res) => {
+    console.log("Recuperando a categoria com id ", req.params.id);
+
+    category
+        .findOne({_id:ObjectId(req.params.id)})
+            .then(result => {
+                res.status(200).send(result);
+            }).catch(error => {
+                res.status(500).send("Error: "+ error);
+            });
+});
+
+app.put('/category/:id', (req,res) => {
+    console.log("Alterando a categoria com id ", req.params.id);
+
+    category
+        .updateOne({_id:ObjectId(req.params.id)}, req.body)
+            .then(result => {
+                res.status(200).send(result);
+            }).catch(error => {
+                res.status(500).send("Error: "+ error);
+            });
+});
+
